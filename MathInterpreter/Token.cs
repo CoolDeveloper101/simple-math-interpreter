b83486edfbaa151f9c6895219c0c64b12f1ce4a4
Ashwin Gupta <cooldeveloper101@gmail.com>
2020-05-07 15:02:51 +0530

    Adding an enum called TokenType which will hold the types of tokens that can exist.

--- MathInterpreter/Token.cs
+++ MathInterpreter/Token.cs
@@ -4,5 +4,20 @@ namespace MathInterpreter
 {
     public class Token
     {
+        
     }
+
+    public enum TokenType
+    {
+        NUMBER, // The number token type
+        PLUS, // The '+' operator will have the type TokenType.PLUS
+        MINUS, // The '-' operator will have the type TokenType.MINUS
+        MULTIPLY, // The '*' operator will have the type TokenType.MULTIPLY
+        DIVIDE, // The '/' operator will have the type TokenType.DIVIDE
+        POWER, // The '**' operator will have the type TokenType.POWER
+        LPAREN, // The '(' operator will have the type TokenType.LPAREN
+        RPAREN, // The ')' operator will have the type TokenType.RPAREN
+        EOF,
+    }
+
 }
