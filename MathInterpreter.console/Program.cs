3256990b0773f2a2dbde15b058c4cca85c2975c0
Ashwin Gupta <cooldeveloper101@gmail.com>
2020-05-11 12:29:24 +0530

    Completing the console.

--- MathInterpreter.console/Program.cs
+++ MathInterpreter.console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MathInterpreter;
 
 namespace MathInterpreter.console
 {
@@ -6,7 +7,31 @@ namespace MathInterpreter.console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Simple Math Interpreter");
+            Console.ResetColor();
+            while (true)
+            {
+                try
+                {
+                    Console.Write(">>> ");
+                    string expression = Console.ReadLine();
+                    var lexer = new Lexer(expression);
+                    var tokens = lexer.GetTokens();
+                    var parser = new Parser(tokens);
+                    var tree = parser.Parse();
+                    if (tree.nodeType != NodeType.EmptyNode)
+                    {
+                        var interpreter = new Interpreter();
+                        var result = interpreter.Visit(tree);
+                        Console.WriteLine(result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+            }
         }
     }
 }
