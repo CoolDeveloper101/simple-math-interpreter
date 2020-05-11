f921ed206cf9c4288d5623392bcfd0344f11da67
Ashwin Gupta <cooldeveloper101@gmail.com>
2020-05-11 10:42:05 +0530

    Adding the interpreter. Now it is time to write some tests for the interpreter. By the way, I also forgot to switch to the master branch for writing the interpreter.

---
+++ MathInterpreter/Interpreter.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace MathInterpreter
+{
+    class Interpreter
+    {
+        public Number Visit(Node node)
+        {
+            if (node.nodeType == NodeType.NumberNode)
+                return new Number(node.Value);
+            else if (node.nodeType == NodeType.AddNode)
+                return VisitAddNode(node);
+            else if (node.nodeType == NodeType.SubtractNode)
+                return VisitSubtractNode(node);
+            else if (node.nodeType == NodeType.MultiplyNode)
+                return VisitMultiplyNode(node);
+            else if (node.nodeType == NodeType.DivideNode)
+                return VisitDivideNode(node);
+            else if (node.nodeType == NodeType.PowerNode)
+                return VisitPowerNode(node);
+            else if (node.nodeType == NodeType.PlusNode)
+                return VisitPlusNode(node);
+            else if (node.nodeType == NodeType.MinusNode)
+                return VisitMinusNode(node);
+
+            throw new Exception("Invalid Node.");
+        }
+
+        public Number VisitAddNode(Node node)
+        {
+            return new Number(Visit(node.nodeA).Value + Visit(node.nodeB).Value);
+        }
+
+        public Number VisitSubtractNode(Node node)
+        {
+            return new Number(Visit(node.nodeA).Value - Visit(node.nodeB).Value);
+        }
+
+        public Number VisitMultiplyNode(Node node)
+        {
+            return new Number(Visit(node.nodeA).Value * Visit(node.nodeB).Value);
+        }
+
+        public Number VisitDivideNode(Node node)
+        {
+            return new Number(Visit(node.nodeA).Value / Visit(node.nodeB).Value);
+        }
+
+        public Number VisitPowerNode(Node node)
+        {
+            return new Number(Math.Pow(Visit(node.nodeA).Value , Visit(node.nodeB).Value));
+        }
+
+        public Number VisitPlusNode(Node node)
+        {
+            return Visit(node.nodeA);
+        }
+
+        public Number VisitMinusNode(Node node)
+        {
+            return new Number(-1 * Visit(node.nodeA).Value);
+        }
+
+    }
+}
