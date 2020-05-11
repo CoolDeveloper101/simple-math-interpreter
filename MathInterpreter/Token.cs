3c4612dca410fd009e6e852015b300ceae50764f
CoolDeveloper101 <49064764+CoolDeveloper101@users.noreply.github.com>
2020-05-11 19:33:18 +0530

    Fixing a bug in the lambda function.

--- MathInterpreter/Token.cs
+++ MathInterpreter/Token.cs
@@ -38,7 +38,7 @@ namespace MathInterpreter
             }
         }
 
-        public override int GetHashCode() => return base.GetHashCode();
+        public override int GetHashCode() => base.GetHashCode();
 
     }
 
