57141bf8ebcd5ab9287e5d713408ea1ca9ebd158
Ashwin Gupta <cooldeveloper101@gmail.com>
2020-05-11 12:11:21 +0530

    Creating a new console project which will be the console for the math interpreter and adding the project to solution.

---
+++ MathInterpreter.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathInterpreter.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
