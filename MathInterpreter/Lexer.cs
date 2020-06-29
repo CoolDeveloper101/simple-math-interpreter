bad005b47c82a7a80fba71fa7c129923fec74026
Ashwin Gupta <cooldeveloper101@gmail.com>
2020-06-29 09:52:30 +0530

    Removing the unnecessary conversion from char to string as the the lexer should be able to handle chars more easily and also this should improve performance as the lexer previously had to convert each char to a string.

--- MathInterpreter/Lexer.cs
+++ MathInterpreter/Lexer.cs
@@ -31,13 +31,13 @@ namespace MathInterpreter
         /// <param name="text"></param>
         public Lexer(string text)
         {
-            Text = text + "\0"; // Assigning the Text property to the text argument provided by the user and also adding the null character to it so that we can keep a check of whether we are at the end of the file.
+            Text = text + '\0'; // Assigning the Text property to the text argument provided by the user and also adding the null character to it so that we can keep a check of whether we are at the end of the file.
         }
 
         /// <summary>
         /// The current character at which the lexer is.
         /// </summary>
-        public string Current => $"{Text[Index]}";
+        public char Current => Text[Index];
 
         /// <summary>
         /// This method adds 1 to the Lexer.Index variable so that the Lexer.Current method can return the next character from the property Lexer.Text.
@@ -51,29 +51,29 @@ namespace MathInterpreter
         {
             List<Token> tokens = new List<Token>();
 
-            while (Current!= "\0") // Checking whether the current character is the null character which means we are at the end o fthe input.
+            while (Current != '\0') // Checking whether the current character is the null character which means we are at the end o fthe input.
             {
                 if (WHITESPACE.Contains(Current)) // Checking if the current token is space, tab or newline
                     Advance(); // If the current token is in whitespace, we advance.
 
-                else if (DIGITS.Contains(Current) || Current== ".") // We are checking if the current character is a digit.
+                else if (DIGITS.Contains(Current) || Current == '.') // We are checking if the current character is a digit.
                 {
                     tokens.Add(GetNumber()); // If the current character is a digit, we call a method called GetNumber which converts the string to a NumberToken starting from the current character to the charcter which is not a digit or decimal point.
                 }
-                else if (Current== "+") // Checking if the current character is a '+' operator. If it is, we advance and add a Token of TokenType.PLUS
+                else if (Current == '+') // Checking if the current character is a '+' operator. If it is, we advance and add a Token of TokenType.PLUS
                 {
                     Advance();
                     tokens.Add(new Token(TokenType.PLUS));
                 }
-                else if (Current== "-") // Checking if the current character is a '-' operator. If it is, we advance and add a Token of TokenType.MINUS
+                else if (Current == '-') // Checking if the current character is a '-' operator. If it is, we advance and add a Token of TokenType.MINUS
                 {
                     Advance();
                     tokens.Add(new Token(TokenType.MINUS));
                 }
-                else if (Current== "*") // Checking if the current character is a '*' operator
+                else if (Current == '*') // Checking if the current character is a '*' operator
                 {
                     Advance();
-                    if (Current== "*") // Checking if the next charcter is also a '*' operator which converts it into a power operator.
+                    if (Current== '*') // Checking if the next charcter is also a '*' operator which converts it into a power operator.
                     {
                         Advance();
                         tokens.Add(new Token(TokenType.POWER));
@@ -81,17 +81,17 @@ namespace MathInterpreter
                     else // If it is and the next token is not a '*' character, we advance and add a Token of TokenType.MULTIPLY
                         tokens.Add(new Token(TokenType.MULTIPLY));
                 }
-                else if (Current== "/") // Checking if the current character is a '/' operator. If it is, we advance and add a Token of TokenType.DIVIDE
+                else if (Current == '/') // Checking if the current character is a '/' operator. If it is, we advance and add a Token of TokenType.DIVIDE
                 {
                     Advance();
                     tokens.Add(new Token(TokenType.DIVIDE));
                 }
-                else if (Current== "(") // Checking if the current character is a '(' operator. If it is, we advance and add a Token of TokenType.LPAREN
+                else if (Current == '(') // Checking if the current character is a '(' operator. If it is, we advance and add a Token of TokenType.LPAREN
                 {
                     Advance();
                     tokens.Add(new Token(TokenType.LPAREN));
                 }
-                else if (Current== ")") // Checking if the current character is a ')' operator. If it is, we advance and add a Token of TokenType.RPAREN
+                else if (Current == ')') // Checking if the current character is a ')' operator. If it is, we advance and add a Token of TokenType.RPAREN
                 {
                     Advance();
                     tokens.Add(new Token(TokenType.RPAREN));
@@ -131,14 +131,14 @@ namespace MathInterpreter
         public Token GetNumber()
         {
             int decimal_point_count = 0;
-            if (Current== ".")
+            if (Current == '.')
                 decimal_point_count += 1;
-            string number_str = Current; // We initially store the number as a string.
+            string number_str = Current.ToString(); // We initially store the number as a string.
             Advance();
 
-            while (Current!= "\0" && (DIGITS.Contains(Current) || Current== "."))
+            while (Current != '\0' && (DIGITS.Contains(Current) || Current == '.'))
             {
-                if (Current== ".")
+                if (Current == '.')
                 {
                     decimal_point_count += 1;
                     if (decimal_point_count > 1)
@@ -148,10 +148,10 @@ namespace MathInterpreter
                 Advance();
             }
 
-            if (number_str.StartsWith("."))
-                number_str = "0" + number_str;
-            if (number_str.EndsWith("."))
-                number_str += "0";
+            if (number_str.StartsWith('.'))
+                number_str = '0' + number_str;
+            if (number_str.EndsWith('.'))
+                number_str += '0';
 
             return new Token(TokenType.NUMBER, Convert.ToDouble(number_str)); // Converting the string number to a double.
         }
